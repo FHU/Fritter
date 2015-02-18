7bb8b8e993dedfab52bdf1064d27645feada7450
Kenan Casey <kenancasey@gmail.com>
2015-02-18 10:33:25 -0600

    initial commit
    added existing fritter project to repo

---
+++ Fritter/Fritter/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Fritter
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "Account/{controller}/{action}/",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Treat",
+                url: "{userName}",
+                defaults: new { controller = "Treat", action = "Index", userName = UrlParameter.Optional}
+                );
+
+            
+        }
+    }
+}
---
+++ Fritter/Fritter/Models/Treat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fritter.Models
+{
+    public class Treat
+    {
+        public string Text { get; set; }
+        
+        //TO DO: This should be the User class NOT string
+        public string UserName { get; set; }
+        
+        public DateTime Timestamp { get; set; }
+
+        //TO DO: This should be the User class NOT string
+        public List<string> Bites { get; set; }
+        
+        //TO DO: This should be the User class NOT string
+        public List<string> Replies { get; set; }
+
+        public Treat()
+        {
+            this.Timestamp = DateTime.Now;
+        }
+
+        public Treat(string Text = "", string UserName = "")
+        {
+            this.Text = Text;
+            this.UserName = UserName;
+            this.Timestamp = DateTime.Now;
+        }
+    }
+}
